3c555d4ed712a3e45dcf92c9174c8c2ee4e53549
Nibaron Kumar Das <36547410+Nibaron@users.noreply.github.com>
2023-08-19 17:57:15 +0600

    Local Storage used to store state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Addtask from './components/addtask.component';
 import Appbar from './components/appbar.component';
@@ -9,30 +9,14 @@ import './App.css';
 
 function App() {
 
-  const [taskList, setTaskList] = useState([
-      { 
-          id: 1001, 
-          name: "Wake Up Early", 
-          time: "6:00:00 AM 8/19/2023" 
-      },
-      {
-          id: 2001,
-          name: "Practice Leetcode Daily",
-          time: "7:22:19 AM 8/17/2023",
-      },
-      {
-          id: 3001,
-          name: "Complete REACT project",
-          time: "12:09:58 PM 8/12/2023",
-      },
-      {
-        id: 4001,
-        name: "Watch a Movie",
-        time: "10:00:00 PM 8/10/2023",
-    },
-  ]);
+  const [taskList, setTaskList] = useState(
+    JSON.parse(localStorage.getItem("taskList")) || []);
 
   const [task, setTask] = useState({});
+
+  useEffect(()=>{
+    localStorage.setItem("taskList", JSON.stringify(taskList))
+  },[taskList]);
 
   return (
     <>
